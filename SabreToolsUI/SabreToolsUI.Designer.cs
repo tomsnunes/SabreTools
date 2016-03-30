5d43c48a170a5a4668959aa0a7a355d45c7f2e16
Matt Nadareski <mnadareski@outlook.com>
2016-03-30 12:59:38 -0700

    Bump version number

--- SabreToolsUI/SabreToolsUI.Designer.cs
+++ SabreToolsUI/SabreToolsUI.Designer.cs
@@ -261,7 +261,7 @@
 			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "SabreToolsUI";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "SabreTools UI 0.2.2.1";
+			this.Text = "SabreTools UI 0.2.3.0";
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
