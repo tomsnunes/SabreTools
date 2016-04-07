4654c7a1576478cc1b6b9c38c1fa721e559b9d20
Matt Nadareski <mnadareski@outlook.com>
2016-04-07 14:52:28 -0700

    @tractivo: fix NonGood pattern

--- DATabase/Import.cs
+++ DATabase/Import.cs
@@ -25,7 +25,7 @@ namespace SabreTools
 		private static string _noIntroPattern = @"^(.*?) \((\d{8}-\d{6})_CM\)\.dat$";
 		private static string _noIntroNumberedPattern = @"(.*? - .*?) \(\d.*?_CM\).dat";
 		private static string _noIntroSpecialPattern = @"(.*? - .*?) \((\d{8})\)\.dat";
-		private static string _nonGoodPattern = @"(NonGood.*?)( .*)?.dat";
+		private static string _nonGoodPattern = @"^(NonGood.*?)( .*)?.xml";
 		private static string _redumpPattern = @"^(.*?) \((\d{8} \d{2}-\d{2}-\d{2})\)\.dat$";
 		private static string _redumpBiosPattern = @"^(.*?) \(\d+\) \((\d{4}-\d{2}-\d{2})\)\.dat$";
 		private static string _tosecPattern = @"^(.*?) - .* \(TOSEC-v(\d{4}-\d{2}-\d{2})_CM\)\.dat$";
