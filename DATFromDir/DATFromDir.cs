5134bbe4aeca59a08139c8d8805ed4522ad07a23
Matt Nadareski <mnadareski@outlook.com>
2016-04-12 02:16:02 -0700

    Clean includes

--- DATFromDir/DATFromDir.cs
+++ DATFromDir/DATFromDir.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 
 using DamienG.Security.Cryptography;
