ad9a59098dfe10a94fcc7ab5f924845bdf83d820
Matt Nadareski <mnadareski@outlook.com>
2016-05-29 01:58:48 -0700

    Bump version number

--- SabreHelper/Data/Constants.cs
+++ SabreHelper/Data/Constants.cs
@@ -5,7 +5,7 @@
 		/// <summary>
 		/// The current toolset version to be used by all child applications
 		/// </summary>
-		public static string Version = "0.8.0.0";
+		public static string Version = "v0.8.3";
 
 		// 0-byte file constants
 		public static long SizeZero = 0;
