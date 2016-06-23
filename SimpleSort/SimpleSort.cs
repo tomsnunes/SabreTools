c3e55b733476ea8e46c383a5f42d193e4f9a18bc
Matt Nadareski <mnadareski@outlook.com>
2016-06-22 21:24:31 -0700

    [SimpleSort] Tell the user that they're the remaining stats

--- SimpleSort/SimpleSort.cs
+++ SimpleSort/SimpleSort.cs
@@ -321,6 +321,7 @@ namespace SabreTools
 			}
 
 			// Now output the stats for the DAT (remaining)
+			_logger.User("Stats of the remaining ROMs:");
 			Stats.OutputStats(_datdata, _logger, true);
 
 			return success;
