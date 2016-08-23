27c06fa9a09d9cc546345a3c521408378741d43a
Matt Nadareski <mnadareski@outlook.com>
2016-08-23 16:40:56 -0700

    [SabreTools] Add short forms to check

--- SabreTools/Partials/SabreTools_Helpers.cs
+++ SabreTools/Partials/SabreTools_Helpers.cs
@@ -134,49 +134,49 @@ ORDER BY system.manufacturer, system.name";
 
 			// Get any trailing size identifiers
 			long multiplier = 1;
-			if (sizestring.EndsWith("kb"))
+			if (sizestring.EndsWith("k") || sizestring.EndsWith("kb"))
 			{
 				multiplier = Constants.KiloByte;
 			}
-			else if (sizestring.EndsWith("kib"))
+			else if (sizestring.EndsWith("ki") || sizestring.EndsWith("kib"))
 			{
 				multiplier = Constants.KibiByte;
 			}
-			else if (sizestring.EndsWith("mb"))
+			else if (sizestring.EndsWith("m") || sizestring.EndsWith("mb"))
 			{
 				multiplier = Constants.MegaByte;
 			}
-			else if (sizestring.EndsWith("mib"))
+			else if (sizestring.EndsWith("mi") || sizestring.EndsWith("mib"))
 			{
 				multiplier = Constants.MibiByte;
 			}
-			else if (sizestring.EndsWith("gb"))
+			else if (sizestring.EndsWith("g") || sizestring.EndsWith("gb"))
 			{
 				multiplier = Constants.GigaByte;
 			}
-			else if (sizestring.EndsWith("gib"))
+			else if (sizestring.EndsWith("gi") || sizestring.EndsWith("gib"))
 			{
 				multiplier = Constants.GibiByte;
 			}
-			else if (sizestring.EndsWith("tb"))
+			else if (sizestring.EndsWith("t") || sizestring.EndsWith("tb"))
 			{
 				multiplier = Constants.TeraByte;
 			}
-			else if (sizestring.EndsWith("tib"))
+			else if (sizestring.EndsWith("ti") || sizestring.EndsWith("tib"))
 			{
 				multiplier = Constants.TibiByte;
 			}
-			else if (sizestring.EndsWith("pb"))
+			else if (sizestring.EndsWith("p") || sizestring.EndsWith("pb"))
 			{
 				multiplier = Constants.PetaByte;
 			}
-			else if (sizestring.EndsWith("pib"))
+			else if (sizestring.EndsWith("pi") || sizestring.EndsWith("pib"))
 			{
 				multiplier = Constants.PibiByte;
 			}
 
 			// Remove any trailing identifiers
-			sizestring = sizestring.TrimEnd(new char[] { 'k', 'm', 'g', 't', 'p', 'i', 'b' });
+			sizestring = sizestring.TrimEnd(new char[] { 'k', 'm', 'g', 't', 'p', 'i', 'b', ' ' });
 
 			// Now try to get the size from the string
 			if (!Int64.TryParse(sizestring, out size))
