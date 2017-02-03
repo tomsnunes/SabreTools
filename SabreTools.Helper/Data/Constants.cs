1b5d5707994fe11eba478412ae5a2e4b87f652c4
Matt Nadareski <mnadareski@outlook.com>
2017-02-02 23:24:55 -0800

    [Constants] Bump version number, because I can

--- SabreTools.Helper/Data/Constants.cs
+++ SabreTools.Helper/Data/Constants.cs
@@ -10,7 +10,7 @@ namespace SabreTools.Helper.Data
 		/// <summary>
 		/// The current toolset version to be used by all child applications
 		/// </summary>
-		public static string Version = "v0.9.3-" + Assembly.GetExecutingAssembly().GetLinkerTime().ToString("yyyy-MM-dd HH:mm:ss");
+		public static string Version = "v0.9.5-" + Assembly.GetExecutingAssembly().GetLinkerTime().ToString("yyyy-MM-dd HH:mm:ss");
 		public const int HeaderHeight = 3;
 
 		#region 0-byte file constants
