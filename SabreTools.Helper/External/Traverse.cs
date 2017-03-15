2a70e9c3816874e421521693422e184da335902d
Matt Nadareski <mnadareski@outlook.com>
2017-03-14 20:09:44 -0700

    [Traverse] Add edc's traverse code (not hooked up)

---
+++ SabreTools.Helper/External/Traverse.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+#if MONO
+using System.IO;
+#else
+using Alphaleonis.Win32.Filesystem;
+
+using IOException = System.IO.IOException;
+using SearchOption = System.IO.SearchOption;
+#endif
+using SabreTools.Helper.Data;
+
+namespace SabreTools.Helper.External
+{
+	public class Traverse
+	{
+		/// Original version: Microsoft (example code), updated by edc
+		public void TraverseTreeParallelForEach(string root, Action<FileInfo> action)
+		{
+			List<string> dirs = new List<string>();
+
+			if (!Directory.Exists(root))
+			{
+				throw new ArgumentException();
+			}
+
+			dirs.Add(root);
+
+			List<string> subdirs = new List<string>();
+
+			while (dirs.Count > 0 || subdirs.Count > 0)
+			{
+				foreach (string dir in subdirs)
+				{
+					dirs.Add(dir);
+				}
+				subdirs.Clear();
+
+				Parallel.ForEach(dirs,
+					new ParallelOptions() { MaxDegreeOfParallelism = Globals.MaxDegreeOfParallelism },
+					currentDir =>
+				{
+					string[] subDirs = Directory.GetDirectories(currentDir);
+
+					lock (subdirs)
+					{
+						foreach (string str in subDirs)
+						{
+							subdirs.Add(str);
+						}
+					}
+
+					var dir = new DirectoryInfo(currentDir);
+					try
+					{
+						FileInfo[] files = dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+						Parallel.ForEach(files,
+							new ParallelOptions() { MaxDegreeOfParallelism = Globals.MaxDegreeOfParallelism },
+							info =>
+						{
+							action(info);
+						});
+					}
+					catch { }
+				});
+				dirs.Clear();
+			}
+		}
+	}
+}
