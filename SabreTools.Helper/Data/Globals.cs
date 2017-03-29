99a3814d0fd267a8974f210bdaa58f6ad9943f83
Matt Nadareski <mnadareski@outlook.com>
2017-03-29 14:11:36 -0700

    [Globals] Fix exe dir for mono environments

--- SabreTools.Helper/Data/Globals.cs
+++ SabreTools.Helper/Data/Globals.cs
@@ -15,7 +15,9 @@ namespace SabreTools.Helper.Data
 
 		private static Logger _logger = null;
 		private static int _maxDegreeOfParallelism = 4;
-		private static string _exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", "");
+		private static string _exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase)
+			.Replace("file:\\", "")
+			.Replace("file:/", "");
 
 		#endregion
 
