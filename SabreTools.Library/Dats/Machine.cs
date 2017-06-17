59cd3c86e45821230fd04e0478321f5c849377d8
Matt Nadareski <mnadareski@outlook.com>
2017-06-16 17:09:34 -0700

    [Machine] Slim down struct

--- SabreTools.Library/Dats/Machine.cs
+++ SabreTools.Library/Dats/Machine.cs
@@ -9,21 +9,6 @@ namespace SabreTools.Library.Dats
 	{
 		#region Protected instance variables
 
-		// Machine information
-		private string _name;
-		private string _comment;
-		private string _description;
-		private string _year;
-		private string _manufacturer;
-		private string _romOf;
-		private string _cloneOf;
-		private string _sampleOf;
-		private string _sourceFile;
-		private bool? _runnable;
-		private string _board;
-		private string _rebuildTo;
-		private List<string> _devices;
-		private MachineType _machineType;
 		private Guid _guid;
 
 		#endregion
@@ -31,76 +16,20 @@ namespace SabreTools.Library.Dats
 		#region Publicly facing variables
 
 		// Machine information
-		public string Name
-		{
-			get { return _name; }
-			set { _name = value; }
-		}
-		public string Comment
-		{
-			get { return _comment; }
-			set { _comment = value; }
-		}
-		public string Description
-		{
-			get { return _description; }
-			set { _description = value; }
-		}
-		public string Year
-		{
-			get { return _year; }
-			set { _year = value; }
-		}
-		public string Manufacturer
-		{
-			get { return _manufacturer; }
-			set { _manufacturer = value; }
-		}
-		public string RomOf
-		{
-			get { return _romOf; }
-			set { _romOf = value; }
-		}
-		public string CloneOf
-		{
-			get { return _cloneOf; }
-			set { _cloneOf = value; }
-		}
-		public string SampleOf
-		{
-			get { return _sampleOf; }
-			set { _sampleOf = value; }
-		}
-		public string SourceFile
-		{
-			get { return _sourceFile; }
-			set { _sourceFile = value; }
-		}
-		public bool? Runnable
-		{
-			get { return _runnable; }
-			set { _runnable = value; }
-		}
-		public string Board
-		{
-			get { return _board; }
-			set { _board = value; }
-		}
-		public string RebuildTo
-		{
-			get { return _rebuildTo; }
-			set { _rebuildTo = value; }
-		}
-		public List<string> Devices
-		{
-			get { return _devices; }
-			set { _devices = value; }
-		}
-		public MachineType MachineType
-		{
-			get { return _machineType; }
-			set { _machineType = value; }
-		}
+		public string Name;
+		public string Comment;
+		public string Description;
+		public string Year;
+		public string Manufacturer;
+		public string RomOf;
+		public string CloneOf;
+		public string SampleOf;
+		public string SourceFile;
+		public bool? Runnable;
+		public string Board;
+		public string RebuildTo;
+		public List<string> Devices;
+		public MachineType MachineType;
 
 		#endregion
 
@@ -113,20 +42,20 @@ namespace SabreTools.Library.Dats
 		/// <param name="description">Description of the machine</param>
 		public Machine(string name, string description)
 		{
-			_name = name;
-			_comment = null;
-			_description = description;
-			_year = null;
-			_manufacturer = null;
-			_romOf = null;
-			_cloneOf = null;
-			_sampleOf = null;
-			_sourceFile = null;
-			_runnable = null;
-			_board = null;
-			_rebuildTo = null;
-			_devices = null;
-			_machineType = MachineType.NULL;
+			Name = name;
+			Comment = null;
+			Description = description;
+			Year = null;
+			Manufacturer = null;
+			RomOf = null;
+			CloneOf = null;
+			SampleOf = null;
+			SourceFile = null;
+			Runnable = null;
+			Board = null;
+			RebuildTo = null;
+			Devices = null;
+			MachineType = MachineType.NULL;
 			_guid = new Guid();
 		}
 
@@ -182,66 +111,6 @@ namespace SabreTools.Library.Dats
 		public override int GetHashCode()
 		{
 			return OCRC.OptimizedCRC.Compute(_guid.ToByteArray());
-		}
-
-		#endregion
-
-		#region Update fields
-
-		/// <summary>
-		/// Append a string to the description
-		/// </summary>
-		public void AppendDescription(string append)
-		{
-			UpdateDescription(this.Description + append);
-		}
-
-		/// <summary>
-		/// Append a string to the name
-		/// </summary>
-		public void AppendName(string append)
-		{
-			UpdateName(this.Name + append);
-		}
-
-		/// <summary>
-		/// Update the cloneof
-		/// </summary>
-		public void UpdateCloneOf(string update)
-		{
-			_cloneOf = update;
-		}
-
-		/// <summary>
-		/// Update the description
-		/// </summary>
-		public void UpdateDescription(string update)
-		{
-			_description = update;
-		}
-
-		/// <summary>
-		/// Update the romof
-		/// </summary>
-		public void UpdateRomOf(string update)
-		{
-			_romOf = update;
-		}
-
-		/// <summary>
-		/// Update the sampleof
-		/// </summary>
-		public void UpdateSampleOf(string update)
-		{
-			_sampleOf = update;
-		}
-
-		/// <summary>
-		/// Update the name
-		/// </summary>
-		public void UpdateName(string update)
-		{
-			_name = update;
 		}
 
 		#endregion
